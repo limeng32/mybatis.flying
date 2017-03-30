ae36ab6412edc106dbdf9edced11329977315036
limeng32 <limeng32@live.cn>
2017-03-30 16:17:09 +0800

    将PaginationFixInterceptor合并到EnhancedCachingInterceptor中 成为0.1.2版本

--- src/main/java/indi/mybatis/flying/interceptors/EnhancedCachingInterceptor.java
+++ src/main/java/indi/mybatis/flying/interceptors/EnhancedCachingInterceptor.java
@@ -1,24 +1,39 @@
 package indi.mybatis.flying.interceptors;
 
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 
+import org.apache.ibatis.cache.Cache;
 import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.cache.TransactionalCacheManager;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.factory.DefaultObjectFactory;
+import org.apache.ibatis.reflection.factory.ObjectFactory;
+import org.apache.ibatis.reflection.wrapper.DefaultObjectWrapperFactory;
+import org.apache.ibatis.reflection.wrapper.ObjectWrapperFactory;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.type.TypeHandlerRegistry;
 
 import indi.mybatis.flying.cache.CacheKeysPool;
 import indi.mybatis.flying.cache.EnhancedCachingManager;
 import indi.mybatis.flying.cache.EnhancedCachingManagerImpl;
+import indi.mybatis.flying.models.Conditionable;
+import indi.mybatis.flying.models.Limitable;
+import indi.mybatis.flying.models.Sortable;
 
 @Intercepts(value = {
 		@Signature(args = { MappedStatement.class, Object.class, RowBounds.class,
@@ -31,6 +46,8 @@ public class EnhancedCachingInterceptor implements Interceptor {
 	private static CacheKeysPool queryCacheOnCommit = new CacheKeysPool();
 	private static Set<String> updateStatementOnCommit = new HashSet<String>();
 	private static EnhancedCachingManager cachingManager = EnhancedCachingManagerImpl.getInstance();
+	private static final ObjectFactory DEFAULT_OBJECT_FACTORY = new DefaultObjectFactory();
+	private static final ObjectWrapperFactory DEFAULT_OBJECT_WRAPPER_FACTORY = new DefaultObjectWrapperFactory();
 
 	@Override
 	public Object intercept(Invocation invocation) throws Throwable {
@@ -101,6 +118,22 @@ public class EnhancedCachingInterceptor implements Interceptor {
 			// 记录本次查询所产生的CacheKey
 			CacheKey cacheKey = executor.createCacheKey(mappedStatement, parameter, rowBounds, boundSql);
 			queryCacheOnCommit.putElement(mappedStatement.getId(), cacheKey);
+
+			Executor executorProxy = (Executor) invocation.getTarget();
+			MetaObject metaExecutor = MetaObject.forObject(executorProxy, DEFAULT_OBJECT_FACTORY,
+					DEFAULT_OBJECT_WRAPPER_FACTORY);
+			// 分离代理对象链
+			while (metaExecutor.hasGetter("h")) {
+				Object object = metaExecutor.getValue("h");
+				metaExecutor = MetaObject.forObject(object, DEFAULT_OBJECT_FACTORY, DEFAULT_OBJECT_WRAPPER_FACTORY);
+			}
+			// 分离最后一个代理对象的目标类
+			while (metaExecutor.hasGetter("target")) {
+				Object object = metaExecutor.getValue("target");
+				metaExecutor = MetaObject.forObject(object, DEFAULT_OBJECT_FACTORY, DEFAULT_OBJECT_WRAPPER_FACTORY);
+			}
+
+			return this.query(metaExecutor, args);
 		}
 
 		return result;
@@ -175,5 +208,101 @@ public class EnhancedCachingInterceptor implements Interceptor {
 		if (!cachingManager.isInitialized()) {
 			cachingManager.initialize(properties);
 		}
+	}
+
+	private <E> List<E> query(MetaObject metaExecutor, Object[] args) throws SQLException {
+		MappedStatement mappedStatement = (MappedStatement) args[0];
+		Object parameter = args[1];
+		RowBounds rowBounds = (RowBounds) args[2];
+		ResultHandler resultHandler = (ResultHandler) args[3];
+		BoundSql boundSql = mappedStatement.getBoundSql(parameter);
+		CacheKey cacheKey = createCacheKey(mappedStatement, parameter, rowBounds, boundSql);
+		return this.query(metaExecutor, mappedStatement, cacheKey, parameter, rowBounds, resultHandler, boundSql);
+	}
+
+	@SuppressWarnings("unchecked")
+	private <E> List<E> query(MetaObject metaExecutor, MappedStatement mappedStatement, CacheKey cacheKey,
+			Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) throws SQLException {
+		MetaObject metaParameter = MetaObject.forObject(parameter, DEFAULT_OBJECT_FACTORY,
+				DEFAULT_OBJECT_WRAPPER_FACTORY);
+		// 当需要分页查询时，缓存里加入page信息
+		if (metaParameter.getOriginalObject() instanceof Conditionable) {
+			Cache cache = mappedStatement.getCache();
+			if (cache != null && metaExecutor.hasGetter("delegate")) {
+				if (mappedStatement.isUseCache() && resultHandler == null) {
+					if (!(Boolean) metaExecutor.getValue("dirty")) {
+						cache.getReadWriteLock().readLock().lock();
+						try {
+							synchronized (cache) {
+								Object value = cache.getObject(cacheKey);
+								if (value != null) {
+									HashMap<String, Object> cachedMap = (HashMap<String, Object>) value;
+									Limitable cachedPage = (Limitable) cachedMap.get("limiter");
+									Limitable originalPage = ((Conditionable) metaParameter.getOriginalObject())
+											.getLimiter();
+									if (null != originalPage && null != cachedPage) {
+										originalPage.setTotalCount(cachedPage.getTotalCount());
+										return (List<E>) cachedMap.get("list");
+									}
+								} else {
+								}
+							}
+						} finally {
+							cache.getReadWriteLock().readLock().unlock();
+						}
+					}
+				}
+				Executor delegate = (Executor) metaExecutor.getValue("delegate");
+				List<E> list = delegate.query(mappedStatement, parameter, rowBounds, resultHandler, cacheKey, boundSql);
+				TransactionalCacheManager tcm = (TransactionalCacheManager) metaExecutor.getValue("tcm");
+				HashMap<String, Object> cachedMap = new HashMap<String, Object>();
+				cachedMap.put("limiter", metaParameter.getValue("limiter"));
+				cachedMap.put("list", list);
+				tcm.putObject(cache, cacheKey, cachedMap);
+				return list;
+			}
+		}
+		Executor executor = (Executor) metaExecutor.getOriginalObject();
+		return executor.query(mappedStatement, parameter, rowBounds, resultHandler, cacheKey, boundSql);
+	}
+
+	private CacheKey createCacheKey(MappedStatement mappedStatement, Object parameter, RowBounds rowBounds,
+			BoundSql boundSql) {
+		CacheKey cacheKey = new CacheKey();
+		cacheKey.update(mappedStatement.getId());
+		cacheKey.update(rowBounds.getOffset());
+		cacheKey.update(rowBounds.getLimit());
+		List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+		cacheKey.update(boundSql.getSql());
+		MetaObject metaObject = MetaObject.forObject(parameter, DEFAULT_OBJECT_FACTORY, DEFAULT_OBJECT_WRAPPER_FACTORY);
+
+		if (parameterMappings.size() > 0 && parameter != null) {
+			TypeHandlerRegistry typeHandlerRegistry = mappedStatement.getConfiguration().getTypeHandlerRegistry();
+			if (typeHandlerRegistry.hasTypeHandler(parameter.getClass())) {
+				cacheKey.update(parameter);
+			} else {
+				for (ParameterMapping parameterMapping : parameterMappings) {
+					String propertyName = parameterMapping.getProperty();
+					if (metaObject.hasGetter(propertyName)) {
+						cacheKey.update(metaObject.getValue(propertyName));
+					} else if (boundSql.hasAdditionalParameter(propertyName)) {
+						cacheKey.update(boundSql.getAdditionalParameter(propertyName));
+					}
+				}
+			}
+		}
+		// 当需要分页查询时，将page参数里的当前页和每页数加到cachekey里
+		if (metaObject.getOriginalObject() instanceof Conditionable) {
+			Sortable sorter = ((Conditionable) metaObject.getOriginalObject()).getSorter();
+			if (sorter != null) {
+				cacheKey.update(sorter.toSql());
+			}
+			Limitable limiter = ((Conditionable) metaObject.getOriginalObject()).getLimiter();
+			if (limiter != null) {
+				cacheKey.update(limiter.getPageNo());
+				cacheKey.update(limiter.getPageSize());
+			}
+		}
+		return cacheKey;
 	}
 }
