9a8da43ae587b9adf6c6ff33009d2fa178e2eef1
limeng32 <limeng32@live.cn>
2017-06-24 23:29:52 +0800

    优化pom文件

--- src/main/java/indi/mybatis/flying/utils/ReflectHelper.java
+++ src/main/java/indi/mybatis/flying/utils/ReflectHelper.java
@@ -4,20 +4,21 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+
 public class ReflectHelper {
+	private static final Log logger = LogFactory.getLog(ReflectHelper.class);
+
 	public static Field getFieldByFieldName(Object obj, String fieldName) {
 		for (Class<?> superClass = obj.getClass(); superClass != Object.class; superClass = superClass
 				.getSuperclass()) {
@@ -29,16 +30,6 @@ public class ReflectHelper {
 		return null;
 	}
 
-	/**
-	 * 
-	 * @param obj
-	 * @param fieldName
-	 * @return
-	 * @throws SecurityException
-	 * @throws NoSuchFieldException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 */
 	public static Object getValueByFieldName(Object obj, String fieldName)
 			throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
 		Field field = getFieldByFieldName(obj, fieldName);
@@ -55,16 +46,6 @@ public class ReflectHelper {
 		return value;
 	}
 
-	/**
-	 * 
-	 * @param obj
-	 * @param fieldName
-	 * @param value
-	 * @throws SecurityException
-	 * @throws NoSuchFieldException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 */
 	public static void setValueByFieldName(Object obj, String fieldName, Object value)
 			throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
 		Field field = obj.getClass().getDeclaredField(fieldName);
@@ -78,176 +59,82 @@ public class ReflectHelper {
 	}
 
 	/**
-	 * 
-	 * @param dest
-	 * @param source
-	 * @throws SecurityException
-	 * @throws NoSuchFieldException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 * @throws InvocationTargetException
-	 */
-	public static void copyBean(Object dest, Object source) throws SecurityException, NoSuchFieldException,
-			IllegalArgumentException, IllegalAccessException, InvocationTargetException {
-		Method[] destMethods = dest.getClass().getDeclaredMethods();
-		Method[] sourceMethods = source.getClass().getDeclaredMethods();
-		HashMap<String, Method> destMethodMap = new HashMap<>();
-		HashMap<String, Method> sourceMethodMap = new HashMap<>();
-
-		if (null != sourceMethods && null != destMethods) {
-			for (Method m : sourceMethods) {
-				sourceMethodMap.put(m.getName(), m);
-			}
-			for (Method m : destMethods) {
-				destMethodMap.put(m.getName(), m);
-			}
-		} else {
-			return;
-		}
-		for (Field field : source.getClass().getDeclaredFields()) {
-			if (Modifier.isStatic(field.getModifiers()) || Modifier.isFinal(field.getModifiers())) {
-				continue;
-			}
-			String getMethodName = "get" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
-			String setMethodName = "set" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
-			if (destMethodMap.get(setMethodName) == null || sourceMethodMap.get(getMethodName) == null) {
-				setValueByFieldName(dest, field.getName(), getValueByFieldName(source, field.getName()));
-			} else {
-				Method destMethod = (Method) destMethodMap.get(setMethodName);
-				Method sourceMethod = (Method) sourceMethodMap.get(getMethodName);
-				destMethod.invoke(dest, sourceMethod.invoke(source));
-			}
-		}
-	}
-
-	/**
-	 * 
-	 * @param dest
-	 * @param source
-	 * @throws SecurityException
-	 * @throws NoSuchFieldException
-	 * @throws IllegalArgumentException
-	 * @throws IllegalAccessException
-	 * @throws InvocationTargetException
-	 */
-	public static void copyBeanByNullField(Object dest, Object source) throws SecurityException, NoSuchFieldException,
-			IllegalArgumentException, IllegalAccessException, InvocationTargetException {
-		Method[] destMethods = dest.getClass().getDeclaredMethods();
-		Method[] sourceMethods = source.getClass().getDeclaredMethods();
-		HashMap<String, Method> destMethodMap = new HashMap<>();
-		HashMap<String, Method> sourceMethodMap = new HashMap<>();
-
-		if (null != sourceMethods && null != destMethods) {
-			for (Method m : sourceMethods) {
-				sourceMethodMap.put(m.getName(), m);
-			}
-			for (Method m : destMethods) {
-				destMethodMap.put(m.getName(), m);
-			}
-		} else {
-			return;
-		}
-		for (Field field : source.getClass().getDeclaredFields()) {
-			if (Modifier.isStatic(field.getModifiers()) || Modifier.isFinal(field.getModifiers())) {
-				continue;
-			}
-			String getMethodName = "get" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
-			String setMethodName = "set" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
-			if (destMethodMap.get(setMethodName) == null || sourceMethodMap.get(getMethodName) == null) {
-				if (getValueByFieldName(dest, field.getName()) == null) {
-					setValueByFieldName(dest, field.getName(), getValueByFieldName(source, field.getName()));
-				}
-			} else {
-				Method destGetterMethod = (Method) destMethodMap.get(getMethodName);
-				if (destGetterMethod.invoke(dest) == null) {
-					Method destMethod = (Method) destMethodMap.get(setMethodName);
-					Method sourceMethod = (Method) sourceMethodMap.get(getMethodName);
-					destMethod.invoke(dest, sourceMethod.invoke(source));
-				}
-			}
-		}
-	}
-
-	/**
 	 * 从包package中获取所有的Class
 	 * 
 	 * @param packageName
-	 * @return
+	 *            包路径名称
+	 * @return SetOfClasses
 	 */
 	public static Set<Class<?>> getClasses(String packageName) {
 
-		// 第一个class类的集合
+		/* 第一个class类的集合 */
 		Set<Class<?>> classes = new LinkedHashSet<Class<?>>();
-		// 是否循环迭代
+		/* 是否循环迭代 */
 		boolean recursive = true;
-		// 获取包的名字 并进行替换
+		/* 获取包的名字并进行替换 */
 		String packageDirName = packageName.replace('.', '/');
-		// 定义一个枚举的集合 并进行循环来处理这个目录下的things
+		/* 定义一个枚举的集合 并进行循环来处理这个目录下的things */
 		Enumeration<URL> dirs;
 		try {
 			dirs = Thread.currentThread().getContextClassLoader().getResources(packageDirName);
-			// 循环迭代下去
+			/* 循环迭代下去 */
 			while (dirs.hasMoreElements()) {
-				// 获取下一个元素
+				/* 获取下一个元素 */
 				URL url = dirs.nextElement();
 
 				switch (url.getProtocol()) {
-				// 如果是以文件的形式保存在服务器上
+				/* 如果是以文件的形式保存在服务器上 */
 				case "file":
-					// 获取包的物理路径
+					/* 获取包的物理路径 */
 					String filePath = URLDecoder.decode(url.getFile(), "UTF-8");
-					// 以文件的方式扫描整个包下的文件 并添加到集合中
+					/* 以文件的方式扫描整个包下的文件 并添加到集合中 */
 					findAndAddClassesInPackageByFile(packageName, filePath, recursive, classes);
 					break;
-				// 如果是jar包文件
+				/* 如果是jar包文件 */
 				case "jar":
-					// 定义一个JarFile
+					/* 定义一个JarFile */
 					JarFile jar;
 					try {
-						// 获取jar
+						/* 获取jar */
 						jar = ((JarURLConnection) url.openConnection()).getJarFile();
-						// 从此jar包 得到一个枚举类
+						/* 从此jar包 得到一个枚举类 */
 						Enumeration<JarEntry> entries = jar.entries();
-						// 同样的进行循环迭代
+						/* 同样的进行循环迭代 */
 						while (entries.hasMoreElements()) {
-							// 获取jar里的一个实体 可以是目录 和一些jar包里的其他文件 如META-INF等文件
+							/* 获取jar里的一个实体 可以是目录 和一些jar包里的其他文件 如META-INF等文件 */
 							JarEntry entry = entries.nextElement();
 							String name = entry.getName();
-							// 如果是以/开头的
+							/* 如果是以/开头的 */
 							if (name.charAt(0) == '/') {
-								// 获取后面的字符串
+								/* 获取后面的字符串 */
 								name = name.substring(1);
 							}
-							// 如果前半部分和定义的包名相同
+							/* 如果前半部分和定义的包名相同 */
 							if (name.startsWith(packageDirName)) {
 								int idx = name.lastIndexOf('/');
-								// 如果以"/"结尾 是一个包
+								/* 如果以"/"结尾 是一个包 */
 								if (idx != -1) {
-									// 获取包名 把"/"替换成"."
+									/* 获取包名 把"/"替换成"." */
 									packageName = name.substring(0, idx).replace('/', '.');
 								}
-								// 如果可以迭代下去 并且是一个包
+								/* 如果可以迭代下去 并且是一个包 */
 								if ((idx != -1) || recursive) {
-									// 如果是一个.class文件 而且不是目录
+									/* 如果是一个.class文件 而且不是目录 */
 									if (name.endsWith(".class") && !entry.isDirectory()) {
-										// 去掉后面的".class" 获取真正的类名
+										/* 去掉后面的".class" 获取真正的类名 */
 										String className = name.substring(packageName.length() + 1, name.length() - 6);
 										try {
-											// 添加到classes
+											/* 添加到classes */
 											classes.add(Class.forName(packageName + '.' + className));
 										} catch (ClassNotFoundException e) {
-											// log
-											// .error("添加用户自定义视图类错误
-											// 找不到此类的.class文件");
-											e.printStackTrace();
+											logger.error("添加用户自定义视图类错误 找不到此类的.class文件");
 										}
 									}
 								}
 							}
 						}
 					} catch (IOException e) {
-						// log.error("在扫描用户定义视图时从jar包获取文件出错");
-						e.printStackTrace();
+						logger.error("在扫描用户定义视图时从jar包获取文件出错");
 					}
 					break;
 				}
@@ -269,35 +156,33 @@ public class ReflectHelper {
 	 */
 	private static void findAndAddClassesInPackageByFile(String packageName, String packagePath,
 			final boolean recursive, Set<Class<?>> classes) {
-		// 获取此包的目录 建立一个File
+		/* 获取此包的目录 建立一个File */
 		File dir = new File(packagePath);
-		// 如果不存在或者 也不是目录就直接返回
+		/* 如果不存在或者 也不是目录就直接返回 */
 		if (!dir.exists() || !dir.isDirectory()) {
-			// log.warn("用户定义包名 " + packageName + " 下没有任何文件");
 			return;
 		}
-		// 如果存在 就获取包下的所有文件 包括目录
+		/* 如果存在 就获取包下的所有文件 包括目录 */
 		File[] dirfiles = dir.listFiles(new FileFilter() {
-			// 自定义过滤规则 如果可以循环(包含子目录) 或则是以.class结尾的文件(编译好的java类文件)
+			/* 自定义过滤规则 如果可以循环(包含子目录) 或则是以.class结尾的文件(编译好的java类文件) */
 			public boolean accept(File file) {
 				return (recursive && file.isDirectory()) || (file.getName().endsWith(".class"));
 			}
 		});
-		// 循环所有文件
+		/* 循环所有文件 */
 		for (File file : dirfiles) {
-			// 如果是目录 则继续扫描
+			/* 如果是目录 则继续扫描 */
 			if (file.isDirectory()) {
 				findAndAddClassesInPackageByFile(packageName + "." + file.getName(), file.getAbsolutePath(), recursive,
 						classes);
 			} else {
-				// 如果是java类文件 去掉后面的.class 只留下类名
+				/* 如果是java类文件 去掉后面的.class 只留下类名 */
 				String className = file.getName().substring(0, file.getName().length() - 6);
 				try {
 					classes.add(
 							Thread.currentThread().getContextClassLoader().loadClass(packageName + '.' + className));
 				} catch (ClassNotFoundException e) {
-					// log.error("添加用户自定义视图类错误 找不到此类的.class文件");
-					e.printStackTrace();
+					logger.error("添加用户自定义视图类错误 找不到此类的.class文件");
 				}
 			}
 		}
