af061132ead4dcdd4ef5d48ee24ee9e1c395a674
limeng32 <limeng32@live.cn>
2017-08-14 17:23:38 +0800

    增加若干个测试用例用来证明缓存确实生效

--- src/test/java/indi/mybatis/flying/mapper/RoleMapper.java
+++ src/test/java/indi/mybatis/flying/mapper/RoleMapper.java
@@ -1,6 +1,7 @@
 package indi.mybatis.flying.mapper;
 
 import java.util.Collection;
+import java.util.Map;
 
 import indi.mybatis.flying.annotations.CacheAnnotation;
 import indi.mybatis.flying.annotations.CacheRoleAnnotation;
@@ -42,4 +43,6 @@ public interface RoleMapper extends MapperFace<Role_> {
 	@CacheAnnotation(role = CacheRoleType.Observer)
 	public int count(Role_ t);
 
+	@CacheAnnotation(role = CacheRoleType.Trigger)
+	public int updateDirect(Map<String,Object> m);
 }
