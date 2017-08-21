fe1a9feb59c13cb69fd331988dffb16c86c5b3d6
limeng32 <limeng32@live.cn>
2017-08-21 10:43:02 +0800

    增加AerialView类，用于放置flying当前版本和各分支代码的主要不同之处

---
+++ src/main/java/indi/mybatis/flying/AerialView.java
@@ -0,0 +1,10 @@
+package indi.mybatis.flying;
+
+/* 本类中放置当前flying版本号，和flying各分支代码的主要不同之处 */
+public class AerialView {
+
+	/* flying当前版本 */
+	public static final String FLYING_VERSION = "0.7.2";
+
+	/* 此版本和其它版本主要区别代码如下 */
+}
