a091a5a2baf12e895122b010132637d567784dce
limeng32 <limeng32@live.cn>
2019-12-18 14:39:36 +0800

    Make sonar happy1;

--- src/main/java/indi/mybatis/flying/AerialView.java
+++ src/main/java/indi/mybatis/flying/AerialView.java
@@ -1,10 +1,19 @@
 package indi.mybatis.flying;
 
+/**
+ * 
+ * @date 2019年12月18日 11:56:08
+ *
+ * @author 李萌
+ * @Email limeng32@live.cn
+ * @version
+ * @since JDK 1.8
+ */
 /* The current flying version number and alias are placed in this class. */
 public class AerialView {
 
 	/* Flying current version */
-	public static final String FLYING_VERSION = "0.9.12";
+	public static final String FLYING_VERSION = "0.9.15";
 
 	/* Current version alias */
 	public static final String FLYING_NAME = "AURORA_PRELUDE";
--- src/main/java/indi/mybatis/flying/utils/LogLevel.java
+++ src/main/java/indi/mybatis/flying/utils/LogLevel.java
@@ -3,6 +3,15 @@ package indi.mybatis.flying.utils;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 
+ * @date 2019年12月18日 11:56:08
+ *
+ * @author 李萌
+ * @Email limeng32@live.cn
+ * @version
+ * @since JDK 1.8
+ */
 public enum LogLevel {
 
 	NONE("none"), TRACE("trace"), DEBUG("debug"), WARN("warn"), ERROR("error");
