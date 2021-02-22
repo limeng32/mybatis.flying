4294d2e1bae3e2c82c9af935f3c92b983a9ec569
limeng32 <limeng32@live.cn>
2021-02-22 11:56:20 +0800

    完善useAsSalt的测试用例；

--- src/test/java/indi/mybatis/flying/mapper/PermissionMapper.java
+++ src/test/java/indi/mybatis/flying/mapper/PermissionMapper.java
@@ -1,5 +1,7 @@
 package indi.mybatis.flying.mapper;
 
+import java.util.List;
+
 import indi.mybatis.flying.pojo.Permission;
 
 public interface PermissionMapper {
@@ -10,5 +12,11 @@ public interface PermissionMapper {
 
 	public int insert(Permission t);
 
+	public int insertBatch(List<Permission> l);
+
 	public int update(Permission t);
+
+	public int updatePersistent(Permission t);
+
+	public int updateBatch(List<Permission> l);
 }
--- src/test/java/indi/mybatis/flying/test/PermissionTest.java
+++ src/test/java/indi/mybatis/flying/test/PermissionTest.java
@@ -1,6 +1,8 @@
 package indi.mybatis.flying.test;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -23,11 +25,11 @@ import com.github.springtestdbunit.assertion.DatabaseAssertionMode;
 import com.github.springtestdbunit.dataset.ReplacementDataSetLoader;
 
 import indi.mybatis.flying.Application;
+import indi.mybatis.flying.mapper.PermissionMapper;
 import indi.mybatis.flying.pojo.Account_;
 import indi.mybatis.flying.pojo.Permission;
 import indi.mybatis.flying.pojo.condition.Account_Condition;
 import indi.mybatis.flying.service.AccountService;
-import indi.mybatis.flying.service.PermissionService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
@@ -42,7 +44,7 @@ public class PermissionTest {
 	private AccountService accountService;
 
 	@Autowired
-	private PermissionService permissionService;
+	private PermissionMapper permissionService;
 
 	@Test
 	public void test() {
@@ -84,16 +86,69 @@ public class PermissionTest {
 	@DatabaseTearDown(connection = "dataSource1", type = DatabaseOperation.DELETE_ALL, value = "/indi/mybatis/flying/test/permissionTest/testSalt.datasource.result.xml")
 	public void testSalt() {
 		Permission p = new Permission();
+		p.setId(1);
 		p.setName("ann");
 		p.setSalt("ss");
 		p.setSecret("se");
 		permissionService.insert(p);
 
 		Permission p2 = new Permission();
+		p2.setId(2);
 		p2.setName("bob");
 		permissionService.insert(p2);
 		p2.setSecret("a");
 		p2.setSalt("b");
 		permissionService.update(p2);
+
+		List<Permission> l = new ArrayList<>();
+		Permission p3 = new Permission();
+		p3.setId(3);
+		p3.setName("carl");
+		p3.setSecret("s3");
+		p3.setSalt("salt3");
+		l.add(p3);
+		Permission p4 = new Permission();
+		p4.setId(4);
+		p4.setName("duke");
+		p4.setSecret("s4");
+		p4.setSalt("salt4");
+		l.add(p4);
+		Permission p5 = new Permission();
+		p5.setName("evan");
+		p5.setId(5);
+		l.add(p5);
+		Permission p6 = new Permission();
+		p6.setId(6);
+		p6.setName("frank");
+		l.add(p6);
+		Permission p7 = new Permission();
+		p7.setId(7);
+		p7.setName("green");
+		l.add(p7);
+		Permission p8 = new Permission();
+		p8.setId(8);
+		p8.setName("hank");
+		p8.setSecret("s8");
+		l.add(p8);
+		Permission p9 = new Permission();
+		p9.setId(9);
+		p9.setName("ivy");
+		p9.setSalt("salt99");
+		l.add(p9);
+		permissionService.insertBatch(l);
+
+		List<Permission> l2 = new ArrayList<>();
+		p5.setSalt("salt55");
+		p5.setSecret("s5");
+		l2.add(p5);
+		p6.setSalt("salt66");
+		p6.setSecret("s6");
+		l2.add(p6);
+		permissionService.updateBatch(l2);
+
+		p7.setName(null);
+		p7.setSalt("salt77");
+		p7.setSecret("s7");
+		permissionService.updatePersistent(p7);
 	}
 }
