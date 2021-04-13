d9591a1eedb5d572719e2bee92061401af1ac538
limeng32 <limeng32@live.cn>
2021-04-13 16:22:45 +0800

    成为1.0.12版；

--- src/main/java/indi/mybatis/flying/AerialView.java
+++ src/main/java/indi/mybatis/flying/AerialView.java
@@ -16,8 +16,8 @@ public class AerialView {
 	}
 
 	/* Flying current version */
-	public static final String FLYING_VERSION = "1.0.11";
+	public static final String FLYING_VERSION = "1.0.12";
 
 	/* Current version alias */
-	public static final String FLYING_NAME = "AURORA_11";
+	public static final String FLYING_NAME = "AURORA_12";
 }
